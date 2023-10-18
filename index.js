c9139723ba91579bad515e1e4f085cc8de6d933e
nhasan97 <nh6069325@gmail.com>
2023-10-18 21:44:09 +0600

    installed necessary dependencies, ran the server, connected the database, retrieved brand data for the client side

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+// const brands = [
+//   { name: "Toyota", image: "https://i.ibb.co/55DG6sn/OlRQFW.jpg" },
+//   { name: "Ford", image: "https://i.ibb.co/55DG6sn/OlRQFW.jpg" },
+//   { name: "BMW", image: "https://i.ibb.co/55DG6sn/OlRQFW.jpg" },
+//   { name: "Mercedes-Benz", image: "https://i.ibb.co/55DG6sn/OlRQFW.jpg" },
+//   { name: "Tesla", image: "https://i.ibb.co/55DG6sn/OlRQFW.jpg" },
+//   { name: "Honda", image: "https://i.ibb.co/55DG6sn/OlRQFW.jpg" },
+// ];
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ygecibd.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("automotiveDB");
+    const brandsCollection = database.collection("brands");
+
+    app.get("/brands", async (req, res) => {
+      const cursor = brandsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`server started at port ${port}`);
+});
